1f925634165b7477b64ba2483d3d025301106725
Vladimir20221991 <Dj_vold@mail.ru>
2022-04-21 00:00:16 +0300

    DZ done

---
+++ DZSEMINAR2/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+void showsTheSecondDigit()
+{
+    int num = new Random().Next(100, 1000); 
+    int second = num /10 % 10;
+    Console.Write(num + " -> ");
+    if (num > second)
+    {
+       Console.WriteLine(second);
+    }
+}
+showsTheSecondDigit();
+*/
+
+//Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//Я еще по размышляю как сделать эту задачу, до истины не добрался!!!
+
+
+// Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*  
+void weekday()
+{
+    Console.WriteLine();
+    Console.Write("Введите день недели от 1 до 7: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if(num == -1 || num > 7)
+    {
+        Console.WriteLine();
+        Console.Write (" Введите значение от 1 до 7!");
+    }
+    if(num >= 1 && num <= 5)
+    {
+        Console.WriteLine();
+        Console.Write(num + " NO Рабочий день");
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.Write(num + " YES Ура это выходной");
+    }
+   
+}    
+weekday();*/
