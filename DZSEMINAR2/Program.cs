e5f5dd46236f8c2850086d01184b6aefdfba3233
Vladimir20221991 <Dj_vold@mail.ru>
2022-04-21 17:43:26 +0300

    DZ done

--- DZSEMINAR2/Program.cs
+++ DZSEMINAR2/Program.cs
@@ -14,8 +14,49 @@ showsTheSecondDigit();
 */
 
 //Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-//Я еще по размышляю как сделать эту задачу, до истины не добрался!!!
+/*
+Console.Write("Введите число: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+Console.Write(Num + " -> ");
 
+int TheThirdDigitOutOfThree()
+{
+    
+    if (Num > 1000 && Num < 10000) 
+    {
+        int fourDigit = Num / 10;
+        return fourDigit;
+
+    }
+    else if(Num > 10000 && Num < 100000)
+    {
+        int fiveDigit = Num / 100;
+        return fiveDigit;
+    }
+    else if(Num > 100000 && Num < 1000000)
+    {
+        int sixDigit = Num / 1000;
+        return sixDigit;
+    }
+    else if(Num > 1000000)
+    {
+        int sevenDigit = Num / 10000;
+        return sevenDigit;
+    }
+    return Num;
+}
+int remains =TheThirdDigitOutOfThree();
+if (Num < 100)
+{
+    Console.Write("У числа нет третьей цифры");
+}
+else
+{
+    int a = remains % 10;
+    Console.WriteLine(" The third number -> " + a);
+}
+
+*/
 
 // Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 /*  
