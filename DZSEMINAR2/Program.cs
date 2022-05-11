fe9b058a303582e1c77ea062140d08469eee1d56
Vladimir20221991 <Dj_vold@mail.ru>
2022-05-11 14:51:48 +0300

    DZSEMINAR2

--- DZSEMINAR2/Program.cs
+++ DZSEMINAR2/Program.cs
@@ -11,49 +11,29 @@ showsTheSecondDigit();
 */
 
 //Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-/*
+/*  Console.Clear();
 Console.Write("Введите число: ");
-int Num = Convert.ToInt32(Console.ReadLine());
+int Num = Convert.ToInt32(Console.ReadLine()); Console.WriteLine();
 Console.Write(Num + " -> ");
 
-int TheThirdDigitOutOfThree()
+void TheThirdDigitOutOfThree()
 {
-    
-    if (Num > 1000 && Num < 10000) 
+    while (Num > 1000)
     {
-        int fourDigit = Num / 10;
-        return fourDigit;
-
+        Num = Num / 10;
     }
-    else if(Num > 10000 && Num < 100000)
-    {
-        int fiveDigit = Num / 100;
-        return fiveDigit;
-    }
-    else if(Num > 100000 && Num < 1000000)
-    {
-        int sixDigit = Num / 1000;
-        return sixDigit;
-    }
-    else if(Num > 1000000)
-    {
-        int sevenDigit = Num / 10000;
-        return sevenDigit;
-    }
-    return Num;
-}
-int remains =TheThirdDigitOutOfThree();
-if (Num < 100)
-{
-    Console.Write("У числа нет третьей цифры");
-}
-else
-{
-    int a = remains % 10;
-    Console.WriteLine(" The third number -> " + a);
+    if (Num > 99)
+        Console.Write(Num % 10 + " - третья цифра в числе.");
+    else
+        Console.Write("У числа нет третьей цифры");
 }
 
+TheThirdDigitOutOfThree();
+Console.WriteLine(); Console.WriteLine();
 */
+
+
+
 
 // Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 /*  
