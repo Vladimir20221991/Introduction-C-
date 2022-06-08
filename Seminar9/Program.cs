cd6205f83a72aaa813a1845fb9d207baf4f90a7a
Vladimir20221991 <Dj_vold@mail.ru>
2022-06-08 18:11:26 +0300

    Seminar9

---
+++ Seminar9/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 1: Пример.
+/*Console.WriteLine("Введите число: ");
+int num=Convert.ToInt32(Console.ReadLine());
+*/
+/*
+Recursiya(5);
+void Recursiya(int n)
+{
+    if(n>=1)
+    {
+        Recursiya(n-1);
+        Console.Write(n+ " ");
+    }
+}
+*/
+/*
+int Recursiya(int n)
+{
+    if(n<=1) return 1;
+    else
+    return n * n;
+}
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"{Recursiya()}");
+}
+Console.WriteLine("Введите число: ");
+int num=Convert.ToInt32(Console.ReadLine());
+*/
+
+
+
